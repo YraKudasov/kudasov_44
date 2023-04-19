3186098483726019307fb208d8846cd303680f61
Yra Kudasov <kudasov5@mail.ru>
2023-04-19 21:10:02 +0300

    Добавлены 3 функции: - fillArrayWithOneValue (Применяется для заполнения графа проверки дорог нулями, и для заполнения графа минимальной стоимости бесконечностями) - RelaxationOfGraphEdge (Релаксация ребра после прохождения алгоритма дейкстры) - DijkstrasAlgorithm (Нахождение минимальной стоимости из города 1 в город n)

--- kudasov_44/kudasov_44.cpp
+++ kudasov_44/kudasov_44.cpp
@@ -10,12 +10,52 @@
 
 const int infinity = 100000;
 
+void fillArrayWithOneValue(int array[100], int countStrings, int value){
+    for (int z = 1; z <= countStrings; z++) {
+        array[z] = value;
+    }
+}
+
+int roads[100][100], isRoadUsed[100], minAmountOfRoad[100], costOfRoad[100];
+
+void RelaxationOfGraphEdge(int i, int j) {
+    if (minAmountOfRoad[i] + roads[i][j] < minAmountOfRoad[j]) {
+        minAmountOfRoad[j] = minAmountOfRoad[i] + roads[i][j];
+    }
+}
+
+void DijkstrasAlgorithm(int countStrings){
+
+    minAmountOfRoad[1] = 0;
+
+    for (int i = 1; i < countStrings; i++) {
+        int minAmount = infinity, w = -1;
+        for (int j = 1; j <= countStrings; j++) {
+            if (isRoadUsed[j] == 0 && minAmountOfRoad[j] < minAmount) {
+                minAmount = minAmountOfRoad[j];
+                w = j;
+            }
+        }
+
+        if (w < 0) {
+            printf("Минимального пути из 1 в n-ый город не существует");
+            break;
+        }
+
+        isRoadUsed[w] = 1;
+
+        for (int j = 1; j <= countStrings; j++) {
+            if (isRoadUsed[j] == 0 && roads[w][j] != infinity) {
+                RelaxationOfGraphEdge(w, j);
+            }
+        }
+    }
+}
 
 int main()
 {
     setlocale(LC_ALL, "Rus");
     int n, m;
-    int roads[100][100], isRoadUsed[100], minAmountOfRoad[100], costOfRoad[100];
     
     printf("Введите кол-во городов: ");
     scanf("%d", &n);
@@ -42,5 +82,19 @@ int main()
         roads[b][a] = costOfRoad[b];
     }
 
+    fillArrayWithOneValue(isRoadUsed, n, 0);
+
+    fillArrayWithOneValue(minAmountOfRoad, n, infinity);
+
+    DijkstrasAlgorithm(n);
+
+    if (minAmountOfRoad[n] == infinity) {
+        printf("Минимального пути из 1 в n-ый город не существует\n");
+    }
+    else {
+        printf("%d\n", minAmountOfRoad[n]);
+    }
+
+    return 0;
 }
 
