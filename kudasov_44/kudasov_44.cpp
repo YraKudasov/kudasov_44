fa4c1e0cf34e1a88ccbd8a5228fdbf537b9662c5
Yra Kudasov <kudasov5@mail.ru>
2023-05-18 00:23:10 +0300

    1) Теперь данные вводятся в строку в функции readDataFromCsv 2) Подготовительная фаза проходит теперь в main

--- kudasov_44/kudasov_44.cpp
+++ kudasov_44/kudasov_44.cpp
@@ -16,23 +16,23 @@ void fillArrayWithOneValue(int array[MAX_SIZE], int countStrings, int value){
     }
 }
 
-int roads[MAX_SIZE][MAX_SIZE], isRoadUsed[MAX_SIZE], minAmountOfRoad[MAX_SIZE], costOfRoad[MAX_SIZE];
 
-void RelaxationOfGraphEdge(int i, int j) {
-    if (minAmountOfRoad[i] + roads[i][j] < minAmountOfRoad[j]) {
-        minAmountOfRoad[j] = minAmountOfRoad[i] + roads[i][j];
+
+void RelaxationOfGraphEdge(int i, int j, int ways[MAX_SIZE][MAX_SIZE], int minAmountOfWay[MAX_SIZE]) {
+    if (minAmountOfWay[i] + ways[i][j] < minAmountOfWay[j]) {
+        minAmountOfWay[j] = minAmountOfWay[i] + ways[i][j];
     }
 }
 
-void DijkstrasAlgorithm(int countStrings){
+void DijkstrasAlgorithm(int countStrings, int ways[MAX_SIZE][MAX_SIZE], int isWayUsed[MAX_SIZE], int minAmountOfWay[MAX_SIZE]){
 
-    minAmountOfRoad[1] = 0;
+    minAmountOfWay[1] = 0;
 
     for (int i = 1; i < countStrings; i++) {
         int minAmount = infinity, w = -1;
         for (int j = 1; j <= countStrings; j++) {
-            if (isRoadUsed[j] == 0 && minAmountOfRoad[j] < minAmount) {
-                minAmount = minAmountOfRoad[j];
+            if (isWayUsed[j] == 0 && minAmountOfWay[j] < minAmount) {
+                minAmount = minAmountOfWay[j];
                 w = j;
             }
         }
@@ -42,56 +42,57 @@ void DijkstrasAlgorithm(int countStrings){
             break;
         }
 
-        isRoadUsed[w] = 1;
+        isWayUsed[w] = 1;
 
         for (int j = 1; j <= countStrings; j++) {
-            if (isRoadUsed[j] == 0 && roads[w][j] != infinity) {
-                RelaxationOfGraphEdge(w, j);
+            if (isWayUsed[j] == 0 && ways[w][j] != infinity) {
+                RelaxationOfGraphEdge(w, j, ways, minAmountOfWay);
             }
         }
     }
 }
 
-int readDateFromCsv(char* data[], int roads[MAX_SIZE][MAX_SIZE])
+int readDataFromCsv(char* data[], char readDataMassive[1024], size_t sizeDataMassive)
 {
-    int num_cities;
-    int num_edges = 0;
+    
+    printf("%d", sizeDataMassive);
    FILE* infile = fopen(data[0], "r");
 
     if (infile == NULL) {
         printf("Error opening file121212.%s\n", data[0]);
         return 1;
     }
-    
-    printf("Введите кол-во городов: ");
-    fscanf(infile, "%d", &num_cities);
 
-    int sqr_num_cities = num_cities * num_cities;
     
-    for (int i = 1; i <= num_cities; i++) {
-        fillArrayWithOneValue(roads[i], num_cities, infinity);
-    }
+    size_t num_chars_read = 0;
+    char* pos = readDataMassive;
 
-    while (num_edges <= sqr_num_cities) {
-        int source, target, weight;
-        if (fscanf(infile, "%d;%d;%d", &source, &target, &weight) != 3) {
+    while (fgets(pos, sizeDataMassive - num_chars_read, infile)) {
+        
+        size_t len = strlen(pos);
+        num_chars_read += len;
+        pos += len;
+
+        
+        if (num_chars_read == sizeDataMassive - 1) {
             break;
         }
-        
-        if (roads[source][target] == infinity) {
-            roads[source][target] = weight;
-            num_edges++;
-            
-        }
     }
+
+   
+    readDataMassive[num_chars_read] = '\0';
+    printf("%s\n", readDataMassive);
+
     fclose(infile);
-    return num_cities;
+   
+    return 0;
+    
 }
 
-int writeResultToTxt(char* data[], int minAmountOfRoad[MAX_SIZE], int num_cities)
+int writeResultToTxt(char* data[], int minAmountOfWay[MAX_SIZE], int num_cities)
     {
         FILE* outfile;
-        if (minAmountOfRoad[num_cities] == infinity) {
+        if (minAmountOfWay[num_cities] == infinity) {
         printf("Минимального пути из 1 в n-ый город не существует\n");
         }
         else {
@@ -102,34 +103,74 @@ int writeResultToTxt(char* data[], int minAmountOfRoad[MAX_SIZE], int num_cities
             return 1;
         }
 
-        fprintf(outfile, "%d", minAmountOfRoad[num_cities]);
+        fprintf(outfile, "%d", minAmountOfWay[num_cities]);
         fclose(outfile);
          }
         return 0;
 }
 
+int roads[MAX_SIZE][MAX_SIZE], isRoadUsed[MAX_SIZE], minAmountOfRoad[MAX_SIZE];
+
 int main(int argc, char* argv[])
 {
     setlocale(LC_ALL, "Rus");
-    
-    
-    int count_cities;
-    
+    char inputDataStr[1024];
+    size_t sizeInputMassive = sizeof(inputDataStr);
+    int count_cities=0;
+
+
    
     if (argc < 3) { 
         printf("Необходимо передать имена входного и выходного файлов в качестве аргументов\n");
         return 1;
     }
 
-    count_cities = readDateFromCsv(&argv[1], roads);
+    int result_input = readDataFromCsv(&argv[1], inputDataStr, sizeInputMassive);
+    printf("%s\n", inputDataStr);
+    
+  
+
+    sscanf(inputDataStr, "%d", &count_cities);
+    printf("%d", count_cities);
+
+    int sqr_count_cities = count_cities * count_cities;
+
+    for (int i = 1; i <= count_cities; i++) {
+        fillArrayWithOneValue(roads[i], count_cities, infinity);
+    }
+
+    int count_edges = 0;
+    char* pos = inputDataStr;
+    pos = strchr(pos, '\n');
+   
+
+    while (count_edges < sqr_count_cities) {
+        int source=0, target=0, weight=0;
+        if (sscanf(pos, "%d;%d;%d", &source, &target, &weight) != 3) {
+            break;
+        }
+
+        printf("%d,%d,%d\n", source, target, weight);
+        if (roads[source][target] == infinity) {
+            roads[source][target] = weight;
+            count_edges++;
+        }
+        pos = strchr(pos, '\n');
+        
+        if (pos == NULL) {
+            break;
+        }
+        pos++;  // Пропускаем символ '\n'.
+    }
+    
     
     fillArrayWithOneValue(isRoadUsed, count_cities, 0);
 
     fillArrayWithOneValue(minAmountOfRoad, count_cities, infinity);
 
-    DijkstrasAlgorithm(count_cities);
+    DijkstrasAlgorithm(count_cities, roads, isRoadUsed, minAmountOfRoad);
 
-    int result = writeResultToTxt(&argv[2], minAmountOfRoad, count_cities);
+    int result_output = writeResultToTxt(&argv[2], minAmountOfRoad, count_cities);
     
     return 0;
     
